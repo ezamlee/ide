c17d03bc7701d7a0a05fe945a3bd71664b8fc3e0
ezamlee <a.m.essam1992@gmail.com>
2017-05-31 22:34:03 +0200

    init files

---
+++ src/objects/button.js
@@ -0,0 +1,34 @@
+  button = {
+      "desc":{
+        "id"    : "1",
+        "class" : "button",
+        "desc"  : "This is simple button"
+      },
+      "ui":{
+        "skeleton" : ``,
+        "css"      :{
+          "button":{
+            "class"  :["btn","btn-default"],
+            "color"  :"blue"
+          }
+        }
+      },
+      get data (){
+        return   {
+          "title":"select button"
+        }
+      },
+      "provider":{
+        "functions":{
+          "sayHi":function(name){
+            return `Hi, ${name}`
+          },
+          "init":function(){
+            button.ui.skeleton = `<button>${button.data.title}</button>`
+          }
+        },
+        "events":{
+          "click":["sayHi","ahmed"]
+        }
+      }
+  }
